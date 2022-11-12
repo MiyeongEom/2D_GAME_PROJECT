d9397e72a7dfed7b31a963f7c978afcd90c8936f
MiyeongEom <aldjd020420@naver.com>
2022-11-12 23:44:23 +0900

    근거리 몬스터

--- 2DGP GAME_Project/Adj_Monster.py
+++ 2DGP GAME_Project/Adj_Monster.py
@@ -1,33 +1,34 @@
 from pico2d import *
+import game_framework
+import game_world
 
-class Hero:
+class Admon:
+
     def add_event(self, event):
         self.q.insert(0, event)
 
-    def handle_event(self, event):  # 주인공이 스스로 이벤트를 처리할 수 있게
-        if (event.type, event.key) in key_event_table:
-            key_event = key_event_table[(event.type, event.key)]
-            self.add_event(key_event) #변환된 내부 이벤트를 큐에 추가
+    def handle_event(self, event):
+        pass
 
     def __init__(self):
-        self.x, self.y = 0, 90
+        self.x, self.y = 200, 230
         self.frame = 0
         self.dir, self.face_dir = 0, 1
-        self.Idle_image = load_image('MC_Idle.png')
-        self.RUN_image = load_image('MC_Run.png')
+        self.mon_image = load_image('MON_Frog.png')
 
-        self.q = []
-        self.cur_state = IDLE
-        self.cur_state.enter(self, None) #맨처음에는 이벤트가 없었기에
 
     def update(self):
-        self.cur_state.do(self)
-
-        if self.q : #q에 뭔가 들어있다면,
-            event = self.q.pop() #이벤트를 가져오고
-            self.cur_state.exit(self) #현재 상태를 나가고, self에 대한 정보는 전달해주고 ^^
-            self.cur_state = next_state[self.cur_state][event] #다음 상태를 계산
-            self.cur_state.enter(self, event)
+        self.frame = (self.frame + 1) % 6
+        self.x += self.dir
+        if (self.x >= self.x - 50):
+            self.dir = -1
+        elif (self.x <= self.x + 50):
+            self.dir = 1
 
     def draw(self):
-        self.cur_state.draw(self)
+
+        if self.dir == 1:  # 오른쪽을 바라보고 있는 상태
+            self.mon_image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y, 150, 150)
+        elif self.dir == -1:
+            self.mon_image.clip_composite_draw(self.frame * 100, 0, 100, 100, 0, 'h', self.x, self.y, 150,
+                                                150)
